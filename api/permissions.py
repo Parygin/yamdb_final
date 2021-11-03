7cf4ddbc34d37951e89bd3028ffc11ba018c510c
Evgeniy Parygin <3964996@gmail.com>
2021-11-03 11:26:21 +0300

    Upd if-const in permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -8,17 +8,15 @@ class IsAdmin(permissions.BasePermission):
 
 class IsStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if (request.method in permissions.SAFE_METHODS
-            or request.user.is_staff  # noqa
-           ):
+        if request.method in permissions.SAFE_METHODS \
+                or request.user.is_staff:
             return True
         return False
 
 
 class IsOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS
-            or obj.author == request.user or request.user.is_moderator  # noqa
-           ):
+        if request.method in permissions.SAFE_METHODS \
+                or obj.author == request.user or request.user.is_moderator:
             return True
         return False
