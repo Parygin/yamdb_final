9f2e8e858fc17e37422bd416ceba4457fcd26165
Evgeniy Parygin <3964996@gmail.com>
2021-11-03 12:03:33 +0300

    Fix models/__init__.py

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,9 +1,9 @@
+from .user import CustomUser
 from .category import Category
 from .comment import Comment
 from .genre import Genre
 from .review import Review
 from .title import Title
-from .user import CustomUser
 
 __all__ = ['CustomUser', 'Category',
            'Title', 'Genre',
