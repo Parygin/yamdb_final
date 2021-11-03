9fdced6d69fc88651b0be31a83ca8fc114140ad2
Evgeniy Parygin <3964996@gmail.com>
2021-11-03 11:23:18 +0300

    Fix bracket to permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -10,7 +10,7 @@ class IsStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if (request.method in permissions.SAFE_METHODS
             or request.user.is_staff  # noqa
-        ):
+           ):
             return True
         return False
 
@@ -19,6 +19,6 @@ class IsOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         if (request.method in permissions.SAFE_METHODS
             or obj.author == request.user or request.user.is_moderator  # noqa
-        ):
+           ):
             return True
         return False
